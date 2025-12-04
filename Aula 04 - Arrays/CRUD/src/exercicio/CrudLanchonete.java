bbef14a66aaddba46f890610cdf0cdf6ebb93e8b
Diego Eiti Nakashima Fortunato <diegoeiti96@gmail.com>
2025-12-04 11:20:34 -0300

    Finalizando CRUD lanchonete

--- Aula 04 - Arrays/CRUD/src/exercicio/CrudLanchonete.java
+++ Aula 04 - Arrays/CRUD/src/exercicio/CrudLanchonete.java
@@ -48,6 +48,8 @@ public class CrudLanchonete {
                     } else if (opcao == 3){
                         pedidos.add("X-Tudo");
                     }
+                    System.out.println("Pedido realizado com sucesso!");
+                    break;
 
                 case 2:
                     if (pedidos.isEmpty()){
@@ -57,14 +59,37 @@ public class CrudLanchonete {
                         for (int i = 0; i < pedidos.size(); i++) {
                             System.out.println(i + " " + pedidos.get(i));
                         }
-                        }
+                        } break;
 
                 case 3:
-                    System.out.println("-----Atualizar Cadastro-----");
+                    System.out.println("-----Atualizar Pedido-----");
                     System.out.println("Digite o ID do pedido:");
-                    int idAluno = sc.nextInt();
+                    int idPedido = sc.nextInt();
+                    sc.nextLine();
 
-                    System.out.println("Digite ");
+                    System.out.println("Digite o novo lanche que deseja: ");
+                    int novoLanche = sc.nextInt();
+
+
+                    if  (novoLanche == 0){
+                        pedidos.set(idPedido, "X-Salada");
+                    } else if (novoLanche == 1){
+                        pedidos.set(idPedido, "X-Burguer");
+                    } else if (novoLanche == 2){
+                        pedidos.set(idPedido, "X-Egg");
+                    } else if (novoLanche == 3){
+                        pedidos.set(idPedido, "X-Tudo");
+                    }
+
+                    System.out.println("Pedido atualizado com sucesso!");
+
+                case 4:
+                    System.out.println("\n-----Cancelar Pedido-----");
+                    System.out.println("Digite o ID do pedido que deseja cancelar: ");
+                    int IDpedido = sc.nextInt();
+
+                    pedidos.remove(IDpedido);
+                    System.out.println("Pedido cancelado com sucesso");
             }
 
 
