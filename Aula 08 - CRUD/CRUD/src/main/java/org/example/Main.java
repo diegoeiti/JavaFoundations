7ca9a1197eab75da7ffe881bc5456cbb06d355b1
Diego Eiti Nakashima Fortunato <diegoeiti96@gmail.com>
2025-12-16 16:08:26 -0300

    .

--- Aula 08 - CRUD/CRUD/src/main/java/org/example/Main.java
+++ Aula 08 - CRUD/CRUD/src/main/java/org/example/Main.java
@@ -1,15 +1,46 @@
 package org.example;
 
 import org.example.entidades.*;
-
-import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        GerenciarAlunos gerenciarAlunos = new GerenciarAlunos();
-        Aluno aluno1 = new Aluno("Diego", 10,10);
+            GerenciarAlunos ga = new GerenciarAlunos();
+            Scanner sc = new Scanner(System.in);
+            int opcao;
 
-        //gerenciarAlunos.alterarAluno(2, "DiegoEiti", 10, 1);
-        gerenciarAlunos.listarAlunos();
+            while (true) {
+                System.out.println("-----MENU OPCOES-----");
+                System.out.println("1 - Cadastrar Alunos");
+                System.out.println("2 - Listar Alunos");
+                System.out.println("3 - Editar Aluno");
+                System.out.println("4 - Excluir Aluno");
+                System.out.println("5 - Sair");
+                System.out.println("Escolha uma opcao: ");
+                opcao = sc.nextInt();
+                sc.nextLine();
+
+                switch (opcao) {
+                    case 1 -> {
+                        System.out.println("Digite o nome do aluno: ");
+                        String nome = sc.nextLine();
+                        sc.nextLine();
+
+                        System.out.println("Digite a nota do aluno: ");
+                        double nota = sc.nextDouble();
+                        sc.nextLine();
+
+                        System.out.println("Digite a nota 2 do aluno: ");
+                        double nota2 = sc.nextDouble();
+                        sc.nextLine();
+
+                        Aluno aluno = new Aluno(nome, nota, nota2);
+
+
+
+                    }
+                }
+            }
+
+        }
     }
-}
--- Aula 08 - CRUD/lojaRoupas/src/main/java/org/example/Main.java
+++ Aula 08 - CRUD/lojaRoupas/src/main/java/org/example/Main.java
@@ -3,14 +3,48 @@ package org.example;
 import org.example.entidades.*;
 
 import java.sql.SQLException;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() throws SQLException {
         GerenciadorRoupas gerenciadorRoupas = new GerenciadorRoupas();
-        // Roupa roupa1 = new Roupa("Nike", "Camiseta", "M", 5, 150);
-        // gerenciadorRoupas.atualizarRoupa(2, "Nike", "Blusa", "G", 10, 200);
-        gerenciadorRoupas.listarRoupas();
+        Scanner sc = new Scanner(System.in);
+        int opcao;
+
+        while (true) {
+            System.out.println("-----MENU OPCOES-----");
+            System.out.println("1 - Cadastrar Alunos");
+            System.out.println("2 - Listar Alunos");
+            System.out.println("3 - Editar Aluno");
+            System.out.println("4 - Excluir Aluno");
+            System.out.println("5 - Sair");
+            System.out.println("Escolha uma opcao: ");
+            opcao = sc.nextInt();
+            sc.nextLine();
+
+            switch (opcao) {
+                case 1 -> {
+                    System.out.println("Digite o nome do aluno: ");
+                    String nome = sc.nextLine();
+                    sc.nextLine();
+
+                    System.out.println("Digite a nota do aluno: ");
+                    double nota = sc.nextDouble();
+                    sc.nextLine();
+
+                    System.out.println("Digite a nota 2 do aluno: ");
+                    double nota2 = sc.nextDouble();
+                    sc.nextLine();
+
+                    Aluno aluno = new Aluno(nome, nota, nota2);
+                    gerenciadorRoupas.cadastrarAluno(aluno);
+
+
+                }
+            }
+        }
+
     }
 }
